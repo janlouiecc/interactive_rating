39fb0838d58ad62e3ead6c5b569dd0f00a145655
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2023-01-22 13:05:05 -0700

    completed design for thank you page and added JS selectors

---
+++ main.js
@@ -0,0 +1,9 @@
+const ratingState = document.getElementById('rating-state')
+const thankYouState = document.getElementById('thank-you-state')
+const ratingSelection = document.getElementById('rating-selection')
+
+const ratingButton1 = document.getElementById('rating-button-1')
+const ratingButton2 = document.getElementById('rating-button-2')
+const ratingButton3 = document.getElementById('rating-button-3')
+const ratingButton4 = document.getElementById('rating-button-4')
+const ratingButton5 = document.getElementById('rating-button-5')
