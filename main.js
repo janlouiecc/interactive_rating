47ac7e8031a0f63453ed71017fff41efbfc019be
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2023-01-22 14:20:48 -0700

    added event listeners for rating buttons

--- main.js
+++ main.js
@@ -7,3 +7,36 @@ const ratingButton2 = document.getElementById('rating-button-2')
 const ratingButton3 = document.getElementById('rating-button-3')
 const ratingButton4 = document.getElementById('rating-button-4')
 const ratingButton5 = document.getElementById('rating-button-5')
+const submitButton = document.getElementById('submit-button')
+
+// const handleSubmitClick = () => {
+//   document.getElementById('rating-state').style.display = 'none'
+// }
+// submitButton.addEventListener('click', handleSubmitClick)
+
+let selected = 0
+
+ratingButton1.addEventListener('click', () => {
+  ratingButton1.classList.add('active-rating-button')
+  selected = 1
+})
+
+ratingButton2.addEventListener('click', () => {
+  ratingButton2.classList.add('active-rating-button')
+  selected = 2
+})
+
+ratingButton3.addEventListener('click', () => {
+  ratingButton3.classList.add('active-rating-button')
+  selected = 3
+})
+
+ratingButton4.addEventListener('click', () => {
+  ratingButton4.classList.add('active-rating-button')
+  selected = 4
+})
+
+ratingButton5.addEventListener('click', () => {
+  ratingButton5.classList.add('active-rating-button')
+  selected = 5
+})
