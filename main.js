e36ab4e73b86dea873f4f87dac45b26d57bc182e
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2023-01-22 14:39:09 -0700

    redo event handlers for rating buttons

--- main.js
+++ main.js
@@ -7,36 +7,69 @@ const ratingButton2 = document.getElementById('rating-button-2')
 const ratingButton3 = document.getElementById('rating-button-3')
 const ratingButton4 = document.getElementById('rating-button-4')
 const ratingButton5 = document.getElementById('rating-button-5')
-const submitButton = document.getElementById('submit-button')
 
-// const handleSubmitClick = () => {
-//   document.getElementById('rating-state').style.display = 'none'
-// }
-// submitButton.addEventListener('click', handleSubmitClick)
+const ratingButtons = [
+  ratingButton1,
+  ratingButton2,
+  ratingButton3,
+  ratingButton4,
+  ratingButton5,
+]
 
 let selected = 0
 
+document.getElementById('submit-button').addEventListener('click', () => {
+  if (selected === 0) return
+  ratingState.style.display = 'none'
+})
+
+const undoSelection = () => {
+  ratingButtons.forEach((ratingButton) => {
+    ratingButton.classList.remove('active-rating-button')
+  })
+}
+
 ratingButton1.addEventListener('click', () => {
+  if (selected !== 0) {
+    undoSelection()
+  }
   ratingButton1.classList.add('active-rating-button')
   selected = 1
+  ratingSelection.innerHTML = selected
 })
 
 ratingButton2.addEventListener('click', () => {
+  if (selected !== 0) {
+    undoSelection()
+  }
   ratingButton2.classList.add('active-rating-button')
   selected = 2
+  ratingSelection.innerHTML = selected
 })
 
 ratingButton3.addEventListener('click', () => {
+  if (selected !== 0) {
+    undoSelection()
+  }
   ratingButton3.classList.add('active-rating-button')
   selected = 3
+  ratingSelection.innerHTML = selected
 })
 
 ratingButton4.addEventListener('click', () => {
+  if (selected !== 0) {
+    undoSelection()
+  }
   ratingButton4.classList.add('active-rating-button')
   selected = 4
+  ratingSelection.innerHTML = selected
 })
 
 ratingButton5.addEventListener('click', () => {
+  if (selected !== 0) {
+    undoSelection()
+  }
   ratingButton5.classList.add('active-rating-button')
   selected = 5
+  ratingSelection.innerHTML = selected
 })
